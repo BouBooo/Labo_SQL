5aa174e39e886d026daa00c10d75f7a534ad5724
Florent Nicolas <flonicolas.pro@outlook.fr>
2019-02-28 12:29:38 +0100

    testing join

---
+++ request_1.php
@@ -0,0 +1,58 @@
+<?php
+
+require('_classes/database.php');
+require('assets/head.php');
+
+$showResult = false;
+
+if(isset($_POST['run']))
+{
+    $showResult = true;
+    $db = Database::connect();
+    $request = $db->prepare('SELECT * FROM categories');
+    $request->execute();
+}
+
+?>
+
+<div class="container">
+
+    <h1>Request 1</h1>
+
+    <a class="btn btn-dark" href="index.php">Forward</a>
+
+    <h2>See categories</h2>
+
+    <form action="" method="POST">
+        <input class="btn btn-info" type="submit" name="run" value="Run"/>
+    </form>
+
+
+    <?php
+        if($showResult)
+        {
+    ?>
+            <table class="table table-dark">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Category's name</th>
+                    </tr>
+                </thead>
+                <tbody>
+    <?php   
+            while($data = $request->fetch())
+            {
+    ?>
+                <tr>
+                    <td><?= $data['IdCategory']; ?></td>
+                    <td><?= $data['Name']; ?></td>
+                </tr>
+
+    <?php
+            }  
+        }
+    ?>
+            </table>
+
+</div>
---
+++ request_2.php
@@ -0,0 +1,61 @@
+<?php
+
+require('_classes/database.php');
+require('assets/head.php');
+
+$showResult = false;
+
+if(isset($_POST['run']))
+{
+    $showResult = true;
+    $db = Database::connect();
+    $request = $db->prepare('SELECT c.Name, c.IdChenil, l.locationName FROM chenil c JOIN location l ON c.IdLocation = l.IdLocation');
+    $request->execute();
+    $rows = $request->fetchAll();
+}
+
+?>
+
+<div class="container">
+
+    <h1>Request 1</h1>
+
+    <a class="btn btn-dark" href="index.php">Forward</a>
+
+    <h2>See chenils</h2>
+
+    <form action="" method="POST">
+        <input class="btn btn-info" type="submit" name="run" value="Run"/>
+    </form>
+
+
+    <?php
+        if($showResult)
+        {
+    ?>
+            <table class="table table-dark">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Chenil</th>
+                        <th>Location</th>
+                    </tr>
+                </thead>
+                <tbody>
+    <?php   
+            foreach($rows as $row)
+            {
+    ?>
+                <tr>
+                    <td><?= $row['IdChenil']; ?></td>
+                    <td><?= $row['Name']; ?></td>
+                    <td><?= $row['locationName']; ?></td>
+                </tr>
+
+    <?php
+            }  
+        }
+    ?>
+            </table>
+
+</div>
